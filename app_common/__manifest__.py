a7c092a7dbf2598f9777a1d39742cee085c618aa
Chill <chill421@qq.com>
2025-01-06 14:15:55 +0800

    fix #IBCH86 【以16当前的为基准】app_module_value优化提交至云端ai_chat并处理为odoo10版本

--- app_common/__manifest__.py
+++ app_common/__manifest__.py
@@ -39,7 +39,7 @@
 
 {
     'name': "odooAi Common Util and Tools,欧度智能基础功能及面板",
-    'version': '16.0.25.01.03',
+    'version': '16.0.25.01.06',
     'author': 'odooai.cn',
     'category': 'Extra tools',
     'website': 'https://www.odooai.cn',
--- app_common/models/res_config_settings.py
+++ app_common/models/res_config_settings.py
@@ -13,9 +13,9 @@ class ResConfigSettings(models.TransientModel):
 
     app_saas_ok = fields.Boolean('Enable CN SaaS', default=True, config_parameter='app_saas_ok',
                                  help="Checked to Enable www.odooapp.cn cloud service.")
-    app_saas_common_token = fields.Char('SaaS Common Token', config_parameter='app_saas_common_token')
+    app_saas_db_token = fields.Char('SaaS DB Token', config_parameter='app_saas_db_token')
 
     def set_values(self):
         res = super().set_values()
-        self.env['ir.config_parameter'].set_param('app_saas_common_token', self.app_saas_common_token)
+        self.env['ir.config_parameter'].set_param('app_saas_db_token', self.app_saas_db_token)
         return res
