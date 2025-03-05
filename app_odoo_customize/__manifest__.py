2248042e054bd8f7235113406a80fffe55e1a749
Chill <chill421@qq.com>
2025-03-05 12:13:48 +0800

    upd module name

--- app_odoo_customize/__manifest__.py
+++ app_odoo_customize/__manifest__.py
@@ -22,7 +22,7 @@
 # description:
 
 {
-    'name': '!_odoo18 Tweak OEM Development Enhance.Boost,Customize,Ai Employee,UI,Security,Remove Data All in One-优化提速50+项大全',
+    'name': '!_18odoo Tweak OEM Development Enhance.Boost,Customize,Ai Employee,UI,Security,Remove Data All in One-优化提速50+项大全',
     'version': '18.0.25.03.05',
     'author': 'odooai.cn',
     'category': 'Extra Tools',
