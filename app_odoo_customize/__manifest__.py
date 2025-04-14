93494007ba3c44ef06d4cbcf78fc716717e758c1
Chill <chill421@qq.com>
2025-04-14 15:52:15 +0800

    update views

--- app_odoo_customize/__manifest__.py
+++ app_odoo_customize/__manifest__.py
@@ -23,7 +23,7 @@
 
 {
     'name': '!18odoo Tweak OEM Development Enhance.Boost,Customize,Ai Employee,UI,Security,Remove Data All in One-优化提速50+项大全',
-    'version': '18.0.25.04.03',
+    'version': '18.0.25.04.14',
     'author': 'odooai.cn',
     'category': 'Extra Tools',
     'website': 'https://www.odooai.cn',
