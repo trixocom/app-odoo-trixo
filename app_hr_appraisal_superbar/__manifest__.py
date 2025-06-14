99560e2a1758e26fe9dcd08169fb430fe4e6c461
Ivan Office <guohuadeng@hotmail.com>
2025-06-15 05:12:11 +0800

    update superbar for date format. 处理视图转换时but

---
+++ app_hr_appraisal_superbar/__manifest__.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+# Created on 2018-08-15
+# author: 欧度智能，https://www.odooai.cn
+# email: 300883@qq.com
+# resource of odooai
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+# Odoo16在线用户手册（长期更新）
+# https://www.odooai.cn/documentation/16.0/zh_CN/index.html
+
+# Odoo16在线开发者手册（长期更新）
+# https://www.odooai.cn/documentation/16.0/zh_CN/developer.html
+
+# Odoo13在线用户手册（长期更新）
+# https://www.odooai.cn/documentation/user/13.0/zh_CN/index.html
+
+# Odoo13在线开发者手册（长期更新）
+# https://www.odooai.cn/documentation/13.0/index.html
+
+# Odoo在线中文用户手册（长期更新）
+# https://www.odooai.cn/documentation/user/10.0/zh_CN/index.html
+
+# Odoo10离线中文用户手册下载
+# https://www.odooai.cn/odoo10_user_manual_document_offline/
+# Odoo10离线开发手册下载-含python教程，jquery参考，Jinja2模板，PostgresSQL参考（odoo开发必备）
+# https://www.odooai.cn/odoo10_developer_document_offline/
+# description:
+
+{
+    'name': "HR appraisal Navigator Superbar,员工评价绩效超极搜索导航",
+    'version': '14.0.25.06.15',
+    'author': 'odooai.cn',
+    'category': 'Human Resources',
+    'website': 'https://www.odooai.cn',
+    'license': 'LGPL-3',
+    'sequence': 2,
+    'summary': """
+    Browse employees by departments tree. hr organization chart. 
+    Easy to navigator and browse any data. Support list, kanban, pivot, graph view. 
+    ztree widget. hr Hierarchy organization chart Tree.
+    """,
+    'description': """
+    Superbar, zTree widget. 
+    Advance search with real parent children tree, ListView or KanbanView. parent tree, children tree,
+    eg: Product category tree ,Department tree, stock location tree.
+    按部门查看员工，超级方便的查询。
+    """,
+    'price': 0.00,
+    'currency': 'EUR',
+    'depends': [
+        'app_hr_superbar',
+    ],
+    'images': ['static/description/hr2.gif'],
+    'data': [
+        'views/hr_appraisal_views.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'css': [
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    'js': [
+    ],
+    'post_load': None,
+    'post_init_hook': 'post_init_hook',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
