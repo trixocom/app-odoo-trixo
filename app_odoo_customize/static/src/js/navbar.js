42bcc7faca67bd4f49e94093f3fd7ded67d274f3
Chill <chill421@qq.com>
2025-07-23 19:13:41 +0800

    fix #IC51JL app_odoo_customize在18实现菜单debug功能

--- app_odoo_customize/static/src/js/navbar.js
+++ app_odoo_customize/static/src/js/navbar.js
@@ -6,56 +6,45 @@ import {useEffect, useRef} from '@odoo/owl';
 import { patch } from "@web/core/utils/patch";
 import { renderToString } from "@web/core/utils/render";
 
-patch(NavBar.components.MenuDropdown.prototype, {
+patch(NavBar.components.Dropdown.prototype, {
     setup() {
         super.setup();
-        //todo:  鼠标移开要不显示，当前太妨碍
         useEffect(() => this.addDebugTooltip());
     },
+
     addDebugTooltip() {
         let is_asset = browser.location.search.includes('?debug=assets');
-        if (is_asset) {
-            let dropdownDebugData = this.getDebugData()
-            $(this.rootRef.el).find('.dropdown-toggle')
-                .removeAttr('title')
-                .tooltip(this.getDebugTooltip(dropdownDebugData));
-            var self = this;
-            _.each($(this.rootRef.el).find('.dropdown-menu_group'), function (menuGroup, index) {
-                let $menuGroup = $(menuGroup);
-                let menuGroupDebugData = self.getMenuGroupDebugData($menuGroup);
-                $menuGroup.tooltip(self.getDebugTooltip(menuGroupDebugData));
-            })
+        if (is_asset && this.props && this.props.payload) {
+            const debugData = {
+                id: this.props.payload.id,
+                title: this.props.payload.name,
+                xmlid: this.props.payload.xmlid,
+                sequence: this.props.payload.sequence,
+            };
+
+            const dropdownToggle = document.querySelectorAll(`.dropdown-toggle[data-menu-xmlid="${debugData.xmlid}"], .dropdown-toggle[data-section="${debugData.id}"]`);
+
+            if (dropdownToggle && dropdownToggle.length > 0) {
+                const el = dropdownToggle[0];
+                el.removeAttribute('title');
+                const debugTooltip = {
+                    template: '<div class="tooltip tooltip-field-info" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>',
+                    title: renderToString('Menu.tooltip', debugData),
+                };
+                bindCustomTooltip(el, debugTooltip.title);
+            }
         }
     },
-    getDebugData() {
-        return {
-            title: this.props.payload.name,
-            xmlid: this.props.payload.xmlid,
-            sequence: this.props.payload.sequence,
-        }
-    },
-    getMenuGroupDebugData($menuGroup) {
-        return {
-            title: $menuGroup.data('name'),
-            xmlid: $menuGroup.data('xmlid'),
-            sequence: $menuGroup.data('sequence'),
-        }
-    },
-    getDebugTooltip(debugData) {
-        return {
-            template: '<div class="tooltip tooltip-field-info" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>',
-            title: renderToString('Menu.tooltip', debugData),
-        };
-    }
-})
-NavBar.components.MenuDropdown.props.payload = {
+});
+
+NavBar.components.Dropdown.props.payload = {
     type: Object,
     optional: true,
 };
+
 patch(NavBar.components.DropdownItem.prototype, {
     setup() {
         super.setup();
-        //todo:  鼠标移开要不显示，当前太妨碍
         useEffect(() => this.addDebugTooltip());
     },
     addDebugTooltip() {
@@ -65,9 +54,12 @@ patch(NavBar.components.DropdownItem.prototype, {
             if (!menuDebugData) {
                 return;
             }
-            $(`.dropdown-item[data-menu-xmlid="${menuDebugData.xmlid}"], .dropdown-item[data-section="${menuDebugData.id}"]`)
-                .removeAttr('title')
-                .tooltip(this.getDebugTooltip(menuDebugData));
+            const menuItems = document.querySelectorAll(`.dropdown-item[data-menu-xmlid="${menuDebugData.xmlid}"], .dropdown-item[data-section="${menuDebugData.id}"]`);
+            menuItems.forEach(item => {
+                item.removeAttribute('title');
+                const debugTooltip = this.getDebugTooltip(menuDebugData);
+                bindCustomTooltip(item, debugTooltip.title);
+            });
         }
     },
     getDebugData() {
@@ -86,10 +78,31 @@ patch(NavBar.components.DropdownItem.prototype, {
             template: '<div class="tooltip tooltip-field-info" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>',
             title: renderToString('Menu.tooltip', debugData),
         };
-    }
+    },
 })
 
 NavBar.components.DropdownItem.props.payload = {
     type: Object,
     optional: true,
 };
+
+function bindCustomTooltip(element, tooltipHtml) {
+    element.onmouseenter = null;
+    element.onmouseleave = null;
+    element.onmouseenter = function (e) {
+        let tooltip = document.createElement('div');
+        tooltip.className = 'custom-tooltip';
+        tooltip.innerHTML = tooltipHtml;
+        document.body.appendChild(tooltip);
+
+        const rect = element.getBoundingClientRect();
+        const tooltipRect = tooltip.getBoundingClientRect();
+
+        tooltip.style.left = (rect.right + window.scrollX + 8) + 'px';
+        tooltip.style.top = (rect.top + window.scrollY + (rect.height - tooltipRect.height) / 2 + 12) + 'px';
+    };
+    element.onmouseleave = function () {
+        const tooltip = document.querySelector('.custom-tooltip');
+        if (tooltip) tooltip.remove();
+    };
+}
